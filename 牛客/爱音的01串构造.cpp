791826454526b375ad12cf075d3f619123f65de8
afresh-breeze <1526977473@qq.com>
2026-03-26 23:22:10 +0800

    https://ac.nowcoder.com/acm/contest/120564/F

--- 牛客/爱音的01串构造.cpp
+++ 牛客/爱音的01串构造.cpp
@@ -1,60 +1,46 @@
 /*
 https://ac.nowcoder.com/acm/contest/120564/F
 思路解析： 
+mex:子串无0，mex=0
+	子串有0无1，mex=1
+	子串有0有1，mex=2
+字符串总长度n=a+b
+总mex=(mex为1的子串数)*1+(mex为2的子串数)*2
+	 =(包含0的子串数)+(同时包含0和1的子串数) 
+f(0s)为仅由0组成的子串数量
+f(1s)为仅由1组成的子串数量
+总子串数T=n*(n+1)/2
+包含0的子串数=T-f(1s)
+同时包含0和1的子串数=T-f(0s)-f(1s)
+总mex=2T-f(0s)-2*f(1s)
+所以题目要求转化为最小化 f(0s)+2*f(1s)  （也可以直接想成理想情况是每个子串都是2，但是纯1和纯0都会有损失，损失量为此式）
+因为相同的字符连在一起产生的“纯字符子串”的数量是呈平方级增长，所以我们切分得越均匀越好
+1和0中少的字符孤立存在将另一字符尽可能均匀切分 
 */
-#include <iostream>
-#include <string>
-
+#include<iotream>
+#include<string>
 using namespace std;
 
-/**
- * 思路：
- * 总 MEX = (包含0的子串数) + (同时包含0和1的子串数)
- * 设 T 为总子串数，f(0s) 为全0子串数，f(1s) 为全1子串数
- * 总 MEX = (T - f(1s)) + (T - f(0s) - f(1s)) = 2T - f(0s) - 2f(1s)
- * 目标：最小化 f(0s) + 2f(1s)。
- * 通过将数量较多的字符尽可能均匀地由数量较少的字符隔开来达到目的。
- */
-
-void solve() {
-    int a, b;
-    if (!(cin >> a >> b)) return;
-
-    if (a > b) {
-        // 0 多，用 1 来隔开 0
-        int k = b + 1; // 份数
-        int q = a / k; // 每份基础个数
-        int r = a % k; // 余数分配
-        for (int i = 0; i < k; ++i) {
-            int current_zeros = q + (i < r ? 1 : 0);
-            for (int j = 0; j < current_zeros; ++j) cout << '0';
-            if (i < b) cout << '1'; // 最后一个 0 块后不加 1
-        }
-        cout << '\n';
-    } else {
-        // 1 多或相等，用 0 来隔开 1
-        int k = a + 1;
-        int q = b / k;
-        int r = b % k;
-        for (int i = 0; i < k; ++i) {
-            int current_ones = q + (i < r ? 1 : 0);
-            for (int j = 0; j < current_ones; ++j) cout << '1';
-            if (i < a) cout << '0'; // 最后一个 1 块后不加 0
-        }
-        cout << '\n';
-    }
+void solve()
+{
+	long long a,b;
+	cin>>a>>b;
+	if(a>b)
+	{
+		long long k=b+1;//切成多少份
+		long long q=a/k;//每份多少个
+		long long r=a%k;//剩多少个，分到前r组去
+		for(long long) 
+	}
 }
 
-int main() {
-    // 优化 I/O
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
-    return 0;
+int main()
+{
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	return 0;
 }
-
