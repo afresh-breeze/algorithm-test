b69866d634cbe467f8ed20f3bb987d719024e82c
afresh-breeze <1526977473@qq.com>
2026-05-24 16:34:50 +0800

    未完待续

--- 牛客/小L的数组.cpp
+++ 牛客/小L的数组.cpp
@@ -26,11 +26,21 @@ int main()
 	{
 		for(int j;j<2048;++j)//j为x的值 
 		{
-			if(dp[i][j])
+			if(dp[i][j])//如果本轮x的起始值可以为j 
 			{
 				int x1=max(0,j-a[i]);
 				dp[i+1][x1]=true;//在下一轮操作中，x的起始值可以为x1 
+				int x2=j^b[i];
+				dp[i+1][x2]=true;//在下一轮操作中，x的起始值可以为x2
 			}
+		}
+	}
+	for(int i=2047;i>=0;--i)//找第n轮操作后最大的x 
+	{
+		if(dp[n][i])
+		{
+			cout<<i;
+			break;
 		}
 	}
 	return 0;
