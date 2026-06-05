3d07b32fb8764e5a986b62eed711d426e000af74
afresh-breeze <1526977473@qq.com>
2026-06-05 21:54:50 +0800

    未完待续

--- 牛客/小L的三角尺/小L的三角尺.cpp
+++ 牛客/小L的三角尺/小L的三角尺.cpp
@@ -1,13 +1,22 @@
 /*
 https://ac.nowcoder.com/acm/contest/120566/A
-Ë¼Â·ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Ä¼ï¿½ 
+Ë¼Â·½âÎö¼û¸½´øÎÄµµ 
 */
-//666
 #include<iostream>
 #include<vector>
 using namespace std;
 int main()
 {
+	ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    long n,w;
+    cin>>n>>w;
+    vector<long long> x(n);
+    vector<long long> y(n);
+    for(long i=0;i<n;++i)
+    {
+    	cin>>x[i]>>y[i];
+	}
 	return 0;
 }
 
